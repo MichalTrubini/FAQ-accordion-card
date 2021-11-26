9d56e7d5ac2abc91e3ca4df8f2d86313e0943695
Michal Trubini <mchl.trubini@gmail.com>
2021-11-26 18:27:59 +0100

    js done

--- js/script.js
+++ js/script.js
@@ -6,35 +6,24 @@
 function toggleAnswer(item) {
 
     item.addEventListener('click', function(){
-        let answerThis = item.querySelector('.answer__body');
-        let questionThis = item.querySelector('.question__body');
-        let arrowThis = item.querySelector('.question__arrow');
 
-        let answerAny = document.querySelector('.answer-visible');
-        let questionAny = document.querySelector('.question-selected');
-        let arrowAny = document.querySelector('.arrow-up');
+        let currentAnswer = item.querySelector('.answer__body');
+        let currentQuestion = item.querySelector('.question__body');
+        let currentArrow = item.querySelector('.question__arrow');
 
-        answerThis.classList.toggle('answer-visible');
-        questionThis.classList.toggle('question-selected');
-        arrowThis.classList.toggle('arrow-up');
-
-        if(!answerAny === answerThis) answerAny.classList.remove('answer-visible');
-/*         if (answerThis.classList.contains('answer-visible')) {
-            answerThis.classList.remove('answer-visible');
-            questionThis.classList.remove('question-selected');
-            arrowThis.classList.remove('arrow-up');
-            arrowThis.classList.add('arrow-down');
+        let prevAnswerVisible = document.querySelector('.answer-visible');
+        let prevQuestionVisible = document.querySelector('.question-selected');
+        let prevArrow = document.querySelector('.arrow-up');
+        
+        if (prevAnswerVisible && (prevAnswerVisible.getAttribute('data-selected') !== currentAnswer.getAttribute('data-selected'))) {
+            prevAnswerVisible.classList.remove('answer-visible');
+            prevQuestionVisible.classList.remove('question-selected');
+            prevArrow.classList.remove('arrow-up');
         }
-        else {
-            if (answerAny) answerAny.classList.remove('answer-visible');
-            if (questionAny) questionAny.classList.remove('question-selected');
-            if (arrowAny) arrowAny.classList.remove('arrow-up');
-
-            answerThis.classList.add('answer-visible');
-            questionThis.classList.add('question-selected');
-            arrowThis.classList.remove('arrow-down');
-            arrowThis.classList.add('arrow-up');
-        }; */
+    
+        currentAnswer.classList.toggle('answer-visible');
+        currentQuestion.classList.toggle('question-selected');
+        currentArrow.classList.toggle('arrow-up');
     })
 };
 
