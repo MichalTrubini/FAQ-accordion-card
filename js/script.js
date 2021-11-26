9853fb33b880c08636196f16f8b49951fe642860
Michal Trubini <mchl.trubini@gmail.com>
2021-11-26 14:20:14 +0100

    mobile version re-factored as per figma + js WIP

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let cardQuestion = $('.item__wrapper');
+/* let cardQuestion = $('.item__wrapper');
 let cardAnswer = $('.answer__body');
 
 cardAnswer.hide();
@@ -11,4 +11,32 @@ cardQuestion.on('click', function(event){
     $(this).css('font-weight', 700).next().toggle();
 })
 
-console.log(cardQuestion);
+console.log(cardQuestion); */
+
+let questionBlock = document.querySelectorAll('.card__item');
+
+questionBlock.forEach(toggleAnswer);
+
+function toggleAnswer(item) {
+
+    item.addEventListener('click', function(){
+        let answerThis = item.querySelector('.answer__body');
+        let questionThis = item.querySelector('.question__body');
+
+        let answerAny = document.querySelector('.answer-visible');
+        let questionAny = document.querySelector('.question-selected');
+
+        if (answerAny) answerAny.classList.remove('answer-visible');
+        if (questionAny) questionAny.classList.remove('question-selected');
+
+        if (answerThis.classList.contains('answer-visible')) {
+            answerThis.classList.remove('answer-visible');
+            questionThis.classList.remove('question-selected');
+        }
+        else {
+            answerThis.classList.add('answer-visible')
+            questionThis.classList.add('question-selected');
+        };
+    })
+};
+
